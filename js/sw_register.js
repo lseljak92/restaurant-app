6ce1bb14acfa0757d925d7fd56e9e6bbcacafd41
Maria Seljak <lseljak92@gmail.com>
2018-09-15 10:58:21 -0800

    Add Service Worker - Offline first

---
+++ js/sw_register.js
@@ -0,0 +1,10 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+                .register('/sw.js')
+                .then(function(){ 
+                    console.log("Service Worker Registered");
+                })
+                .catch(function(){
+                    console.log("Service Worker Registration failed");
+                });
+}
